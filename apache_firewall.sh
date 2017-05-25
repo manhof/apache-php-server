d90f57e3304f8eeca9f60d01f33df3e6a81697c5
manhof <manhof@users.noreply.github.com>
2017-05-25 14:35:24 -0400

    Add files via upload
    added running files

---
+++ apache_firewall.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+serverbuild= gawk -F= '/^ID=/{print $2}' /etc/os-release
+echo "Firewall Configuration" >> /home/test
+if [[ $serverbuild == *"ubuntu"* ]]
+ then
+	ufw allow in OpenSSH
+    ufw allow in "Apache Full"
+    ufw enable
+    ufw status >> /home/test
+elif [[ $serverbuild == *"centos"* ]]
+ then
+	dnf install firewalld -y
+	firewall-offline-cmd --zone=public --add-interface=eth0
+	firewall-offline-cmd --set-default-zone=public
+	firewall-offline-cmd --zone=public --add-service=ssh
+	firewall-offline-cmd --zone=public --add-service=https
+	firewall-offline-cmd --zone=public --add-service=https	firewall-
+	echo "Default Zone" >> /home/test
+	firewall-offline-cmd --get-default-zone >> /home/test
+	firewall-offline-cmd --info-zone=public >> /home/test
+	systemctl start firewalld
+	systemctl enable firewalld
+else
+	echo "Cannot determine Build Type... Exiting" >> /home/test
+	exit 3
+fi  
+echo "END FIREWALL CONFIG" >> /home/test
---
+++ apache_php.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#apache php configuration setup 
+serverbuild= gawk -F= '/^ID=/{print $2}' /etc/os-release
+if [[ $serverbuild == *"ubuntu"* ]]
+ then
+	rm -rf /etc/apache2/mods-enabled/dir.conf
+    echo "<IfModule mod_dir.c>" >> /etc/apache2/mods-enabled/dir.conf
+    echo "    DirectoryIndex index.php index.html index.cgi index.pl index.xhtml index.htm" >> /etc/apache2/mods-enabled/dir.conf
+    echo "</IfModule>" >> /etc/apache2/mods-enabled/dir.conf
+    systemctl restart apache2 
+    echo "apache updated to look for php first " >> /home/test
+elif [[ $serverbuild == *"centos"* ]]
+ then
+	sed -i -e 's+DirectoryIndex index.html+DirectoryIndex index.html index.php+g' /etc/httpd/conf/httpd.conf
+else
+    echo "Cannot determine Build Type... Exiting" >> /home/test
+	exit 3
+fi	   
