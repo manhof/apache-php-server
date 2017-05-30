768588d67504c17c9bcb497ab452ae91a28d7f7d
manhof <manhof@users.noreply.github.com>
2017-05-30 14:30:46 -0400

    Update apache_php.sh

--- apache_php.sh
+++ apache_php.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 #apache php configuration setup 
-serverbuild= gawk -F= '/^ID=/{print $2}' /etc/os-release
+gawk -F= '/^ID=/{print $2}' /etc/os-release > /home/id.txt
+serverbuild=$(cat /home/id.txt)
+echo " This is the Server Build: " $serverbuild >> /home/test
 if [[ $serverbuild == *"ubuntu"* ]]
  then
 	rm -rf /etc/apache2/mods-enabled/dir.conf
