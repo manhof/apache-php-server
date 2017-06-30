d22f82d9f4b7e89bae95b08820108535a0a9555c
manhof <manhof@users.noreply.github.com>
2017-06-30 08:12:36 -0400

    Update apache_php.sh

--- apache_php.sh
+++ apache_php.sh
@@ -5,16 +5,16 @@ serverbuild=$(cat /home/id.txt)
 echo " This is the Server Build: " $serverbuild >> /home/test
 if [[ $serverbuild == *"ubuntu"* ]]
  then
-	rm -rf /etc/apache2/mods-enabled/dir.conf
-    echo "<IfModule mod_dir.c>" >> /etc/apache2/mods-enabled/dir.conf
-    echo "    DirectoryIndex index.php index.html index.cgi index.pl index.xhtml index.htm" >> /etc/apache2/mods-enabled/dir.conf
-    echo "</IfModule>" >> /etc/apache2/mods-enabled/dir.conf
-    systemctl restart apache2 
-    echo "apache updated to look for php first " >> /home/test
+  rm -rf /etc/apache2/mods-enabled/dir.conf
+  echo "<IfModule mod_dir.c>" >> /etc/apache2/mods-enabled/dir.conf
+  echo "    DirectoryIndex index.php index.html index.cgi index.pl index.xhtml index.htm" >> /etc/apache2/mods-enabled/dir.conf
+  echo "</IfModule>" >> /etc/apache2/mods-enabled/dir.conf
+  systemctl restart apache2 
+  echo "apache updated to look for php first " >> /home/test
 elif [[ $serverbuild == *"centos"* ]]
  then
-	sed -i -e 's+DirectoryIndex index.html+DirectoryIndex index.html index.php+g' /etc/httpd/conf/httpd.conf
+  sed -i -e 's+DirectoryIndex index.html+DirectoryIndex index.html index.php+g' /etc/httpd/conf/httpd.conf
 else
-    echo "Cannot determine Build Type... Exiting" >> /home/test
-	exit 3
+ echo "Cannot determine Build Type... Exiting" >> /home/test
+ exit 3
 fi	   
