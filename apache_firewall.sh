966413a082b1507aff5f35f96db09155389f800d
manhof <manhof@users.noreply.github.com>
2017-06-30 08:13:11 -0400

    Update apache_firewall.sh

--- apache_firewall.sh
+++ apache_firewall.sh
@@ -5,25 +5,25 @@ echo " This is the Server Build: " $serverbuild >> /home/test
 echo "Firewall Configuration" >> /home/test
 if [[ $serverbuild == *"ubuntu"* ]]
  then
-	ufw allow in OpenSSH
-    ufw allow in "Apache Full"
-    ufw enable
-    ufw status >> /home/test
+  ufw allow in OpenSSH
+  ufw allow in "Apache Full"
+  ufw enable
+  ufw status >> /home/test
 elif [[ $serverbuild == *"centos"* ]]
  then
-	dnf install firewalld -y
-	firewall-offline-cmd --zone=public --add-interface=eth0
-	firewall-offline-cmd --set-default-zone=public
-	firewall-offline-cmd --zone=public --add-service=ssh
-	firewall-offline-cmd --zone=public --add-service=https
-	firewall-offline-cmd --zone=public --add-service=https	firewall-
-	echo "Default Zone" >> /home/test
-	firewall-offline-cmd --get-default-zone >> /home/test
-	firewall-offline-cmd --info-zone=public >> /home/test
-	systemctl start firewalld
-	systemctl enable firewalld
+  dnf install firewalld -y
+  firewall-offline-cmd --zone=public --add-interface=eth0
+  firewall-offline-cmd --set-default-zone=public
+  firewall-offline-cmd --zone=public --add-service=ssh
+  firewall-offline-cmd --zone=public --add-service=https
+  firewall-offline-cmd --zone=public --add-service=https	firewall-
+  echo "Default Zone" >> /home/test
+  firewall-offline-cmd --get-default-zone >> /home/test
+  firewall-offline-cmd --info-zone=public >> /home/test
+  systemctl start firewalld
+  systemctl enable firewalld
 else
-	echo "Cannot determine Build Type... Exiting" >> /home/test
-	exit 3
+  echo "Cannot determine Build Type... Exiting" >> /home/test
+  exit 3
 fi  
 echo "END FIREWALL CONFIG" >> /home/test
